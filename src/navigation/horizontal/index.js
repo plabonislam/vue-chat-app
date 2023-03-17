5dbc8a0198fefd4eef3c9984a5f2a1f2bcd3d66d
plabonislam <shahnurislamplabon@gmail.com>
2023-03-18 03:22:46 +0600

    initila project

---
+++ src/navigation/horizontal/index.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    title: 'Home',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-smart-home' },
+  },
+  {
+    title: 'Second pager',
+    to: { name: 'second-page' },
+    icon: { icon: 'tabler-file' },
+  },
+ 
+]
